b47721344feb92086020d51cf2422d4806a6903a
Natalie Diebold <nnd23@cornell.edu>
2014-12-14 20:34:19 -0500

    attempting to solve line bug

--- 3DPaint/src/awtGUI/Brush.java
+++ 3DPaint/src/awtGUI/Brush.java
@@ -102,40 +102,63 @@ public class Brush {
 		return this.image;
 	}
 	
+	private int getNearestByteIndex(int x) {
+		int a = x/4;
+		int b = (a + 1);
+		int diffA = Math.abs(a*4 - x);
+		int diffB = Math.abs(b*4 - x);
+		if (diffA == diffB) {
+			return x;
+		} else {
+			if (diffA < diffB) {
+				return (int)Math.floor(a);
+			} else {
+				return (int)Math.floor(b);
+			}
+		}
+		
+	}
+	
 	public ByteBuffer getByteBuffer(int offX, int offY, int oldW, int oldH, ByteBuffer oldBuffer){
 		int width = image.getWidth();
 		int height = image.getHeight();
 		ByteBuffer bb = NativeMem.createByteBuffer(size * size * 4);
-		//System.out.println(oldBuffer.capacity() +" ::: " + ((offX*oldH+offY*oldH)*2));
-		int i = (offX*oldH + offY*oldW)*2;//oldBuffer.capacity() - 1;
+		int i = (offX*oldH + offY*oldW)*2;//getNearestByteIndex((offX*oldH + offY*oldW)*2);
+		//if (i%4 != 0) System.out.println("UHOH");
 		for(int y = 0;y < size;y++) {
 			int texY = (int)((y + 0.5f) / (size) * height);
 			for(int x = 0;x < size;x++) {
 				int texX = (int)((x + 0.5f) / (size) * width);
 				int pixel = image.getRGB(texX,texY);
-				
+				//System.out.println(texX + ", " + texY);
 	    		float a2 = ((pixel >> 24) & 0xFF);
 	    		float r2 = PaintCanvas.activeColor.R;
 	    		float g2 = PaintCanvas.activeColor.G;
 	    		float b2 = PaintCanvas.activeColor.B;
 	    		//System.out.println(r2+","+g2+","+b2+","+a2+ " :: " + texX + ", " + texY);
 	    		
-	    		//float a1 = oldBuffer.get(i);
+	    		float a1 = oldBuffer.get(i+3);
 	    	    float r1 = oldBuffer.get(i);
 	    	    float g1 = oldBuffer.get(i+1);
 	    	    float b1 = oldBuffer.get(i+2);
 	    	    //System.out.println(r1+","+g1+","+b1+","+a1+ " :: " + texX + ", " + texY);
 	    	    
 	    	    float r,g,b,a;
-	    	    	a = 255;
+	    	    
+	    	    if (a1 == r1 && r1 == g1 && g1 == b1 && b1 != 0f && b1 != -1f) {
+	    	    	//System.out.println(texX + ", " + texY);
+	    	    	a = r = g = b = 0f;	    	    	
+	    	    } else {
+	    	    	a = 255f;
 	    	    	r = (r2 * a2 / 255 + r1 * (-255 + a2));
 	    	    	g = (g2 * a2 / 255 + g1 * (-255 + a2));
 	    	    	b = (b2 * a2 / 255 + b1 * (-255 + a2));
-
-	    		bb.put((byte)r); //oldBuffer.put(i, (byte)r);
-				bb.put((byte)g); //oldBuffer.put(i+1, (byte)g);
-				bb.put((byte)b); //oldBuffer.put(i+2, (byte)b); 
-				bb.put((byte)a); //oldBuffer.put(i+3, (byte)a);
+	    	    }
+	    	    
+	    		bb.put((byte)r); oldBuffer.put(i, (byte)r);
+				bb.put((byte)g); oldBuffer.put(i+1, (byte)g);
+				bb.put((byte)b); oldBuffer.put(i+2, (byte)b); 
+				bb.put((byte)a); //oldBuffer.put(i+3, (byte)a1);
 				//System.out.println(r+","+g+","+b+","+a+ " :: " + texX + ", " + texY);
 				i += 4;
 			}
